7cd69e48466926a9df15d87bf509fb1b7205ee3c
Andrés Sánchez Botta <lsb.creativa@gmail.com>
2026-03-07 11:31:02 -0500

    Creocomponente BuscarPelicula con la estructura basica

--- src/components/BuscarPelicula.jsx
+++ src/components/BuscarPelicula.jsx
@@ -1,1 +1,29 @@
+import React from "react";
+import { useState } from "react";
 
+// este componente es para buscar peliculas en la API de OMDB ecibe setPeliculaOMDB que es la funcion para guardar la pelicula encontrada en el estado del App
+
+function BuscarPelicula({ setPeliculaOMDB }) {
+
+
+  // estado para guardar lo que escribe el usuario en el input
+
+  const [busqueda, setBusqueda] = useState("")
+
+  return (
+    <div className="buscar-pelicula">
+      <h3>Buscar pelicula</h3>
+
+      <input
+        type="text"
+    placeholder="Escribe el nombre de la pelicula..."
+        value={busqueda}
+        onChange={  (e) =>  setBusqueda(e.target.value)}
+      />
+
+      <button>Buscar</button>
+    </div>
+  )
+}
+
+export default BuscarPelicula;
