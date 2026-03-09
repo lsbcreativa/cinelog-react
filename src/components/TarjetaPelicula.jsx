9f976d64eeba27af2e495792354fa84476d98dd4
Andrés Sánchez Botta <lsb.creativa@gmail.com>
2026-03-09 10:09:29 -0500

    Creo TarjetaPelicula con estructura basica y console.log para probar

---
+++ src/components/TarjetaPelicula.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+// tarjeta individual de cada pelicula
+
+function TarjetaPelicula({ pelicula, eliminarPelicula, setPeliculaEditar }) {
+
+  // vamos a ver que nos llega en la prop pelicula
+  console.log(pelicula)
+
+  return (
+    <div className="tarjeta-pelicula">
+      <p>aca va la tarjeta</p>
+    </div>
+  )
+}
+
+export default TarjetaPelicula
