8592f54cf29ef16e0d92a73b959fc17ae5286cb6
Andrés Sánchez Botta <lsb.creativa@gmail.com>
2026-03-09 11:40:43 -0500

    Creo componente ListaPeliculas que recorre el array y renderiza las tarjetas

---
+++ src/components/ListaPeliculas.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+// importamos la tarjeta de pelicula
+import TarjetaPelicula from "./TarjetaPelicula"
+
+  // este componente recibe el array de peliculas y las muestra en una lista ambien recibe las funciones de eliminar y editar para pasarselas a cada tarjeta
+
+function ListaPeliculas({ peliculas, eliminarPelicula, setPeliculaEditar }) {
+
+  return (
+    <div className="lista-peliculas">
+      <h3>Mis peliculas</h3>
+
+      {   /* recorremos el array de peliculas y por cada una renderizamos una TarjetaPelicula */}
+
+  {peliculas.map((pelicula) => (
+        <TarjetaPelicula
+            key={pelicula.id}
+        pelicula={pelicula}
+          eliminarPelicula={eliminarPelicula}
+          setPeliculaEditar={setPeliculaEditar}
+
+        />
+      ))
+      }
+    </div>
+  )
+}
+
+export default ListaPeliculas
