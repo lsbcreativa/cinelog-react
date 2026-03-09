1ebe0359f025436cacc672901c3ce782dba289e5
Andrés Sánchez Botta <lsb.creativa@gmail.com>
2026-03-09 11:18:15 -0500

    Saco console.log y agregamos datos y botones con react-icons

--- src/components/TarjetaPelicula.jsx
+++ src/components/TarjetaPelicula.jsx
@@ -1,16 +1,37 @@
 import React from "react"
 
+// importo los iconos de react-icons para los botones de editar y eliminar
+import { FaEditar, FaTrash } from "react-icons/fa"
 
-// tarjeta individual de cada pelicula
+// tarjeta individual de cada pelicula ca mostramos los datos y los botones para editar o eliminar
 
 function TarjetaPelicula({ pelicula, eliminarPelicula, setPeliculaEditar }) {
 
-  // vamos a ver que nos llega en la prop pelicula
-  console.log(pelicula)
-
   return (
     <div className="tarjeta-pelicula">
-      <p>aca va la tarjeta</p>
+
+      {/* si tiene poster lo mostramos */}
+      {pelicula.poster && (
+        <img src={pelicula.poster} alt={pelicula.titulo} />
+      )}
+
+      <h4>{pelicula.titulo}</h4>
+      <p>{pelicula.descripcion}</p>
+      <span className="estado">{pelicula.estado}</span>
+
+      <div className="botones-tarjeta">
+  {/* boton para editar */}
+
+        <button onClick={() => setPeliculaEditar(pelicula)}>
+          <FaEditar /> Editar
+        </button>
+
+        {/* boton para eliminar */}
+
+        <button onClick={() => eliminarPelicula(pelicula.id)}>
+          <FaTrash /> Eliminar
+        </button>
+      </div>
     </div>
   )
 }
